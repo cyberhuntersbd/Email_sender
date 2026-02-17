dfb40bc80c1ac50a4a0f84fbc51e6b4e9ac87e03
CYBER HUNTERS BD - CHB <cyberhuntersbd@gmail.com>
2026-02-17 12:31:08 +0600

    Update bulkemailsender.py

--- bulkemailsender.py
+++ bulkemailsender.py
@@ -327,7 +327,7 @@ def main():
             print(f"{y}👋 Goodbye!{stp}")
             break
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     try:
         main()
     except KeyboardInterrupt:
